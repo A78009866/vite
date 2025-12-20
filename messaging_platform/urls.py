6f9ed007f4c780d5e78ca46543565d85c71594ed
A78009866 <ibispaintxarabic@gmail.com>
2025-12-20 20:13:09 +0100

    Update urls.py

--- messaging_platform/urls.py
+++ messaging_platform/urls.py
@@ -5,9 +5,9 @@ from vite import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.splash, name='splash'),
-    path('', include('vite.urls')),
+    path('', views.splash, name='splash'), # الصفحة الترحيبية
     path('home/', views.home, name='home'),
-    path('login/', auth_views.LoginView.as_view(template_name='social/login.html'), name='login'),
-    path('vite/', include('vite.urls')),
+    path('register/', views.register, name='register'), # رابط التسجيل
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='social/login.html'), name='login'),
+    path('vite/', include('vite.urls')), # تأكد أن ملف vite/urls.py يحتوي على المسارات الداخلية
 ]
